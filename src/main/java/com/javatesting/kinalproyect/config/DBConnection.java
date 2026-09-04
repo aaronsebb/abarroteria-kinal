bbd3d4ba6757e028e9caa32b4a567dd89c665b75
Informatica <emmanuelkinal533@gmail.com>
2026-09-04 17:07:33 -0600

    implementacion del repositorio

--- src/main/java/com/javatesting/kinalproyect/config/DBConnection.java
+++ src/main/java/com/javatesting/kinalproyect/config/DBConnection.java
@@ -4,10 +4,17 @@
  */
 package main.java.com.javatesting.kinalproyect.config;
 
-/**
- *
- * @author informatica
- */
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
 public class DBConnection {
+    private static Connection connection;
     
+    public static Connection getConnection() throws SQLException{
+        if(connection == null || connection .isClosed()){
+           connection = DriverManager.getConnection(Credentials.URL_DB, Credentials.USER_DB, Credentials.PASS_DB);
+        }
+        return connection;
+    }
 }
