faefa57ca9f48bdc7eaaa7f3d8ed160cd8b0d54c
Informatica <emmanuelkinal533@gmail.com>
2026-09-04 15:42:28 -0600

    Capa config implementada

---
+++ src/main/java/com/javatesting/kinalproyect/config/DBConnection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.javatesting.kinalproyect.config;
+
+/**
+ *
+ * @author informatica
+ */
+public class DBConnection {
+    
+}
