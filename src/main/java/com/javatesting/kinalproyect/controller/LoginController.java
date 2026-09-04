4f9b1d66827f6748416059f61733e7612cc2350b
NeryCoy <jguerra-2026492@kinal.edu.gt>
2026-09-04 16:22:23 -0600

    Implementacion de manejo de alertas

--- src/main/java/com/javatesting/kinalproyect/controller/LoginController.java
+++ src/main/java/com/javatesting/kinalproyect/controller/LoginController.java
@@ -8,8 +8,12 @@ import main.java.com.javatesting.kinalproyect.service.AuthService;
 import main.java.com.javatesting.kinalproyect.util.SceneManager;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 /**
  * XDML Controller class
  *
@@ -19,21 +23,51 @@ public class LoginController implements Initializable {
 
     private final AuthService authService;
     private final SceneManager sceneManager;
+    
+    //clase alert temporal
+    Alert showAlert = new Alert(AlertType.INFORMATION);
+    
+   
+    
     private String commitChange;
+    
+    @FXML
+    private Button btnRegistrar;
+    
+    @FXML 
+    private Button btnIniciarSesion;
+    
+    @FXML 
+    private TextField txtFieldEmail;
+    
+    @FXML 
+    private TextField txtFieldContrasena;
+    
+    
     
     public LoginController(AuthService authService, SceneManager sceneManager) {
         this.authService = authService;
         this.sceneManager = sceneManager;
+        
+        
     }
     
-    
 
-    
-    
-    
+       
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+   
+        public void handleLogin()throws Exception{
+        if(txtFieldEmail.getText().isBlank()||txtFieldEmail.getText().isEmpty()){
+        showAlert.setTitle("Error");
+        showAlert.setHeaderText("Error en al loguearse");
+        showAlert.setContentText("Las cajas de texto no pueden estar vacias");
+        }
+        
+    }     
+    }
     
-}
+    
+
