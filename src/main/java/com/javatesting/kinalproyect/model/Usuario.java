44178cd608b956651174a06481651b8799d209ed
unknown <kevinajurod@gmail.com>
2026-09-07 22:11:03 -0600

    feature: implementación del BCrypt (carpetas)

---
+++ src/main/java/com/javatesting/kinalproyect/model/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.javatesting.kinalproyect.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Usuario {
+    public String nombre;
+    
+    
+}
