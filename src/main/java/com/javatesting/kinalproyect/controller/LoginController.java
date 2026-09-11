ef1ec5dc5bed9a5afa0ed4ba281c22fbf4d475e5
aaronsebb <aaronsebmol@icloud.com>
2026-09-11 13:41:30 -0600

    -

--- src/main/java/com/javatesting/kinalproyect/controller/LoginController.java
+++ src/main/java/com/javatesting/kinalproyect/controller/LoginController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import main.java.com.javatesting.kinalproyect.service.usuario.AuthService;
 public class LoginController implements Initializable {
 
     private final AuthService authService;
