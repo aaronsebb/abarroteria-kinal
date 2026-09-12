b1e3477bb6cba74c3520d852508d50194ee74c73
unknown <jguerra-2026492@kinal.edu.gt>
2026-09-11 20:00:36 -0600

    cambios implementeados

--- src/main/java/com/javatesting/kinalproyect/config/Credentials.java
+++ src/main/java/com/javatesting/kinalproyect/config/Credentials.java
@@ -2,14 +2,14 @@ package main.java.com.javatesting.kinalproyect.config;
 
 public class Credentials {
     
-    /*private static final String URL_DB = "jdbc:mysql://localhost:3306/abarroteria_kinal_in4bv";
-    private static final String USER_DB = "root";
-    private static final String PASS_DB = "8486";
+    /*private static final String URL_DB = "";
+    private static final String USER_DB = "";
+    private static final String PASS_DB = "";
     */
     
-    public static final String URL_DB = "jdbc:mysql://localhost:3306/abarroteria_kinal_in4bv";
-    public static final String  USER_DB = "root";
-    public static final String  PASS_DB = "8486";
+    public static final String URL_DB = System.getenv("varibleEntorno");
+    public static final String  USER_DB = System.getenv("varibleEntorno");
+    public static final String  PASS_DB = System.getenv("varibleEntorno");
     
     private Credentials() {
     }
